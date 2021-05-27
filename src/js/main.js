0493cfed12953d6ee6dfb6954f0a6eebb4795dc8
Azra Efendic <azra_efendic@live.com>
2021-05-27 15:01:53 +0200

    Added a web component base and styling

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,17 @@
 import {animatedScrollTo} from './es6-scroll-to.js';
 
-document.querySelector('.title').innerHTML = 'Great success!'
-document.querySelector('.text').innerHTML = 'Javascript is working!'
+// document.querySelector('.title').innerHTML = 'Great success!'
+// document.querySelector('.text').innerHTML = 'Javascript is working!'
+
+let header = document.querySelector('.main-conversion-headline');
+let button = document.querySelector('.content-button');
+let headline = document.querySelector(".content-headline");
+
+button.addEventListener('click', () => {
+    console.log("Clicked!!");
+    header.innerHTML = 'CtA - Main conversion - Red Headline';
+    headline.style.color = "red";
+} )
+
 
 animatedScrollTo(500);
